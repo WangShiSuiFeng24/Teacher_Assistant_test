10170254954db6e4f364ec8e930372e4b90ed078
Mac <mac@MacdeMac-mini-3.local>
2019-10-30 17:47:25 +0800

    添加运行时RECORD_AUDIO权限

--- app/src/main/java/com/example/teacher_assistant_test/Main3Activity.java
+++ app/src/main/java/com/example/teacher_assistant_test/Main3Activity.java
@@ -1,11 +1,15 @@
 package com.example.teacher_assistant_test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -37,6 +41,7 @@ import java.util.List;
 
 public class Main3Activity extends AppCompatActivity {
     private static final String TAG = "Main3Activity";
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     private List<Student> studentList = new ArrayList<>();
 
     private RecognizerDialog mIatDialog = null;
@@ -102,15 +107,40 @@ public class Main3Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mIatDialog = new RecognizerDialog(Main3Activity.this, null);
+                // Here, thisActivity is the current activity
+                if (ContextCompat.checkSelfPermission(Main3Activity.this,
+                        Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(Main3Activity.this,
+                            Manifest.permission.RECORD_AUDIO)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(Main3Activity.this,
+                                new String[]{Manifest.permission.RECORD_AUDIO},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    // Permission has already been granted
+                    mIatDialog = new RecognizerDialog(Main3Activity.this, null);
 
 //                if(mIatDialog!=null)
-                mIatDialog.setParameter(SpeechConstant.VAD_EOS, "3000");
+                    mIatDialog.setParameter(SpeechConstant.VAD_EOS, "3000");
 //                mIatDialog.setParameter(SpeechConstant.ACCENT, "mandarin");
 
-                mIatDialog.setListener(mRecognizerDialogListener);
+                    mIatDialog.setListener(mRecognizerDialogListener);
 
-                mIatDialog.show();
+                    mIatDialog.show();
+                }
             }
         });
 
@@ -120,6 +150,28 @@ public class Main3Activity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         StudentAdapter studentAdapter = new StudentAdapter(studentList);
         recyclerView.setAdapter(studentAdapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 
     private void initStudent() {
