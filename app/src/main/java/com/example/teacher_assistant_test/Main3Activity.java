92650f47e9b82cfb87446d89bfb2354512b6b131
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2019-11-07 22:04:40 +0800

    重写dispatchTouchEvent方法，在其中判断是否需要隐藏软键盘，实现点击空白处自动隐藏输入法

--- app/src/main/java/com/example/teacher_assistant_test/Main3Activity.java
+++ app/src/main/java/com/example/teacher_assistant_test/Main3Activity.java
@@ -19,8 +19,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -639,6 +641,42 @@ public class Main3Activity extends AppCompatActivity {
         //mResultText.setSelection(mResultText.length())
     }
 
+    // 获取点击事件
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // TODO Auto-generated method stub
+        if(ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View view = getCurrentFocus();
+            if(isHideInput(view, ev)) {
+                HideSoftInput(view.getWindowToken());
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+    // 判定是否需要隐藏
+    private boolean isHideInput(View v, MotionEvent ev) {
+        if(v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left
+                    + v.getWidth();
+            if(ev.getX() > left && ev.getX() < right && ev.getY() > top
+                    && ev.getY() < bottom) {
+                return false;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+    // 隐藏软键盘
+    private void HideSoftInput(IBinder token) {
+        if(token != null) {
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(token,
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
 
     //SQLite查询test_id记录总数
     public long getCount() {
