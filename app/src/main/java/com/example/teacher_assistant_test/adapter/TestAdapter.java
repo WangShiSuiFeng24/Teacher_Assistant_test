87eb0ccb02c800b8c9ffc41fcecc9d4a1eed3742
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2019-11-16 17:05:07 +0800

    鉴于第一次使用本app时主页面为空，交互不太友好，于是重写RecyclerView.Adapter，给RecyclerView设置空布局

--- app/src/main/java/com/example/teacher_assistant_test/adapter/TestAdapter.java
+++ app/src/main/java/com/example/teacher_assistant_test/adapter/TestAdapter.java
@@ -13,8 +13,15 @@ import com.example.teacher_assistant_test.bean.Test;
 
 import java.util.List;
 
-public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
+public class TestAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
     private List<Test> mList;
+
+    /**
+     * viewType--分别为item以及空view
+     */
+    private static final int VIEW_TYPE_ITEM = 1;
+    private static final int VIEW_TYPE_EMPTY = 0;
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 //        TextView test_id;
@@ -27,7 +34,7 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
         }
     }
 
-    public TestAdapter(List<Test> mList) {this.mList = mList;}
+    public TestAdapter(List<Test> mList) { this.mList = mList; }
 
     public void remove(int i) {
         mList.remove(i);
@@ -49,7 +56,16 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //在这里根据不同的进行引入不同的布局
+        if(viewType == VIEW_TYPE_EMPTY) {
+            View emptyView = LayoutInflater.from(parent.getContext()).inflate(R.layout.empty_view, parent, false);
+            return new RecyclerView.ViewHolder(emptyView) {
+
+            };
+        }
+
+        //其他的引入正常的
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_item, parent, false);
 //        ViewHolder holder = new ViewHolder(view);
 //        return holder;
@@ -57,31 +73,52 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
-        Test test = mList.get(position);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder recyclerviewHolder, int position) {
+        if(recyclerviewHolder instanceof ViewHolder) {
+            final ViewHolder holder = (ViewHolder) recyclerviewHolder;
+            Test test = mList.get(position);
 //        holder.test_id.setText(String.valueOf(test.getTest_id()));
-        holder.test_name.setText(test.getTest_name());
+            holder.test_name.setText(test.getTest_name());
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int ps = holder.getLayoutPosition();
-                mOnItemClickListener.onItemClick(ps);
-            }
-        });
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int ps = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemClick(ps);
+                }
+            });
 
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                int ps = holder.getLayoutPosition();
-                mOnItemClickListener.onItemLongClick(ps);
-                return false;
-            }
-        });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int ps = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemLongClick(ps);
+                    return false;
+                }
+            });
+        }
+
     }
 
     @Override
     public int getItemCount() {
+        //同时这里也需要添加判断，如果mList.size()为0的话，只引入一个布局，就是emptyView
+        //那么这个RecyclerView的itemCount为1
+        if(mList.size() == 0) {
+            return 1;
+        }
+
+        //如果不为0，按正常的流程跑
         return mList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //在这里进行判断，如果我们的集合长度为0时，我们就使用emptyView的布局
+        if(mList.size() == 0) {
+            return VIEW_TYPE_EMPTY;
+        }
+        //如果有数据，则使用ITEM的布局
+        return VIEW_TYPE_ITEM;
     }
 }
