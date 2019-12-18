9d1ec54d2283cef540ba27a14ef34249b053cbcd
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2019-12-18 22:54:10 +0800

    新建分数段统计FractionalStatisticsActivity，使用约束布局绘制xml文件（已绘制大部分），添加了一个EditText边框xml和左右半圆Button背景xml等。

---
+++ app/src/main/java/com/example/teacher_assistant_test/activity/FractionalStatisticsActivity.java
@@ -0,0 +1,16 @@
+package com.example.teacher_assistant_test.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teacher_assistant_test.R;
+
+public class FractionalStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fractional_statistics);
+    }
+}
