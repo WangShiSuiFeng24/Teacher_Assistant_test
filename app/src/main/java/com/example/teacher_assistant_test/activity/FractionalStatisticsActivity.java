593d65853004aaaebe90f143d5f8fdb6d61eecbc
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2019-12-19 16:13:33 +0800

    修改并测试了分数段统计页面的布局。1、使用一个线性布局嵌套约束布局，将标题栏放入线性布局中，解决透明状态栏的问题。2、去掉所有绝对路径，转为偏移Vertical_bias和Horizontal_bias。3、设置部分线条width或height为0，并分别设置Horizontal_weight或Vertical_weight为1，可使得线条View紧贴相邻控件。4、在colors中添加颜色值，添加一个TextView的背景（上下半圆+边框+背景颜色），解决一些文件命名等问题等

--- app/src/main/java/com/example/teacher_assistant_test/activity/FractionalStatisticsActivity.java
+++ app/src/main/java/com/example/teacher_assistant_test/activity/FractionalStatisticsActivity.java
@@ -2,9 +2,12 @@ package com.example.teacher_assistant_test.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.teacher_assistant_test.R;
+import com.example.teacher_assistant_test.util.ImmersiveStatusBar;
 
 public class FractionalStatisticsActivity extends AppCompatActivity {
 
@@ -12,5 +15,12 @@ public class FractionalStatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fractional_statistics);
+
+        ImmersiveStatusBar.setImmersiveStatusBar(this);
+    }
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, FractionalStatisticsActivity.class);
+        context.startActivity(intent);
     }
 }
