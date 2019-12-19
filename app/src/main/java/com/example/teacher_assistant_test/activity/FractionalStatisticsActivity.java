e2e7871480d0e57730080fd0085faec9cafec54f
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2019-12-19 17:14:16 +0800

    1、规范activity_fractional_statistics.xml文件中控件id命名。2、设置对应Activity禁用横屏。3、在actionStart()静态方法中接收参数test_id。4、Activity实现View.OnClickListener，重写onClick()方法等

--- app/src/main/java/com/example/teacher_assistant_test/activity/FractionalStatisticsActivity.java
+++ app/src/main/java/com/example/teacher_assistant_test/activity/FractionalStatisticsActivity.java
@@ -4,23 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.teacher_assistant_test.R;
 import com.example.teacher_assistant_test.util.ImmersiveStatusBar;
 
-public class FractionalStatisticsActivity extends AppCompatActivity {
+public class FractionalStatisticsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fractional_statistics);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // 禁用横屏
+
         ImmersiveStatusBar.setImmersiveStatusBar(this);
+
+        long test_id = getIntent().getLongExtra("test_id", 0);
+
+
     }
 
-    public static void actionStart(Context context) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_copy_info:
+
+            case R.id.btn_refresh:
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 传进来一个测试id，则可根据测试id查出对应test_time，test_full_score，连表查询出测试
+     * @param context
+     */
+    public static void actionStart(Context context, long test_id) {
         Intent intent = new Intent(context, FractionalStatisticsActivity.class);
+        intent.putExtra("test_id", test_id);
         context.startActivity(intent);
     }
 }
